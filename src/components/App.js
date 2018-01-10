fe42bb45b220d8e5f5dcc6a78e52b3d22b3bde43
Jongmin Kim <kjm141215@gmail.com>
2018-01-09 19:52:58 -0800

    Fetch categories

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+const apiUrl = "http://localhost:3001";
+const headers = {"Authorization": "secret"};
+
 class App extends Component {
+  state = {
+    categories: []
+  }
+
+  componentDidMount() {
+    fetch(`${apiUrl}/categories`, { headers })
+      .then(res => res.json())
+      .then(({ categories }) => this.setState({ categories }))
+  }
+  renderCategories() {
+    if (this.state.categories !== []) {
+      return this.state.categories.map((category, index) => (
+        <li key={index}>{category.name}</li>))
+    }
+  }
   render() {
+    console.log(this.state.categories)
     return (
       <div>
         <h1>Categories</h1>
+        <ul>
+          {this.renderCategories()}
+        </ul>
       </div>
     );
   }
