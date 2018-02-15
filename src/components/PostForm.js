6f711ea5919be0f785adcd640d3335922ad16f29
Jongmin Kim <kjm141215@gmail.com>
2018-02-15 11:25:56 +0900

    Add styling to PostForm

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -52,26 +52,48 @@ class PostForm extends Component {
     const editMode = this.props.mode === "Edit" ? true : false;
 
     return(
-      <form onSubmit={(event) => this.handleSubmit(event)}>
-        <label>Category</label>
-        <select onChange={event => this.updateCategory(event.target.value)} value={category}
-          defaultValue={category}
-          disabled={editMode}
-          required>
-          <option value="">Choose</option>
-          <option value="react" >React</option>
-          <option value="redux" >Redux</option>
-          <option value="udacity" >Udacity</option>
-        </select>
-        <label>Title</label>
-        <input type="text" value={title} onChange={event => this.updateTitle(event.target.value)} required />
-        <label>Author</label>
-        <input type="text" value={author}
-          onChange={event => this.updateAuthor(event.target.value)}
-          disabled={editMode}
-          required />
-        <label>Body</label>
-        <textarea value={body} onChange={event => this.updateBody(event.target.value)} required></textarea>
+      <form onSubmit={(event) => this.handleSubmit(event)} className="post container">
+        <table className="post-form">
+          <tbody>
+            <tr>
+              <th>Category</th>
+              <td>
+                <select onChange={event => this.updateCategory(event.target.value)} value={category}
+                  defaultValue={category}
+                  disabled={editMode}
+                  required>
+                  <option value="">Choose</option>
+                  <option value="react" >React</option>
+                  <option value="redux" >Redux</option>
+                  <option value="udacity" >Udacity</option>
+                </select>
+              </td>
+
+            </tr>
+            <tr>
+              <th>Title</th>
+              <td>
+                <input type="text" value={title} onChange={event => this.updateTitle(event.target.value)} required />
+              </td>
+            </tr>
+            <tr>
+              <th>Author</th>
+              <td>
+                <input type="text" value={author}
+                  onChange={event => this.updateAuthor(event.target.value)}
+                  disabled={editMode}
+                required />
+              </td>
+              </tr>
+            <tr>
+              <th>Body</th>
+              <td>
+                <textarea value={body} onChange={event => this.updateBody(event.target.value)} required></textarea>
+              </td>
+              </tr>
+
+          </tbody>
+        </table>
         <button type="submit" className="update-button">{editMode ? "Update Post" : "Save Post"}</button>
         { fireRedirect == true && <Redirect to="/" />}
         <button type="button" onClick={() => this.handleCancel()}
